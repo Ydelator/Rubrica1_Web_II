29b1b4a236ae5164d327b8a0556f475e52b20050
Ydelator <ygamerzxy24@gmail.com>
2023-08-27 15:00:19 -0500

    modificacion contexto y overview

--- src/contexto/Contexto.jsx
+++ src/contexto/Contexto.jsx
@@ -4,12 +4,9 @@ import { createContext } from 'react'
 export const Contexto = createContext()
 
 export const Datos = ({children}) => {
-    const [favorito1, setFav1] = React.useState(false)
-    const [favorito2, setFav2] = React.useState(false)
-    const [favorito3, setFav3] = React.useState(false)
-    const [favorito4, setFav4] = React.useState(false)
+
   return (
-    <Contexto.Provider value={{favorito1, setFav1, favorito2, setFav2, favorito3, setFav3, favorito4, setFav4}}>
+    <Contexto.Provider value={{}}>
         {children}
     </Contexto.Provider>
   )
--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -1,47 +1,75 @@
-import React from 'react'
-import { Contexto } from '../contexto/Contexto'
+import React, {useState} from 'react'
 
 const Overview = () => {
-  const [favorito1, setFav1] = React.useState(false)
-  const [favorito2, setFav2] = React.useState(false)
-  const [favorito3, setFav3] = React.useState(false)
-  const [favorito4, setFav4] = React.useState(false)
+  
+  const [favorito1, setFav1] = useState(false)
+  const [favorito2, setFav2] = useState(false)
+  const [favorito3, setFav3] = useState(false)
+  const [favorito4, setFav4] = useState(false)
 
   React.useEffect(()=>{
     try {
-      const valor1 = localStorage.getItem("fav1")
-      const valor2 = localStorage.getItem("fav2")
-      const valor3 = localStorage.getItem("fav3")
-      const valor4 = localStorage.getItem("fav4")
-      setFav1(valor1)
-      setFav2(valor2)
-      setFav3(valor3)
-      setFav4(valor4)
-      console.log(valor1)
-      console.log(valor2)
-      console.log(valor3)
-      console.log(valor4)
+      leer()
     } catch (error) {
       console.error(error)
     }
   },[])
 
+  const leer = () =>{
+    console.log(".."+ localStorage.getItem("fav1"))
+    if (localStorage.getItem("fav1")==null) {
+      setFav1(false)
+    } else {
+      const valor1 = JSON.parse(localStorage.getItem("fav1"))
+      setFav1(valor1)
+      console.log(JSON.parse((localStorage.getItem("fav1"))))
+    }
+
+    if (localStorage.getItem("fav2")==null) {
+      setFav2(false)
+    } else {
+      const valor2 = JSON.parse(localStorage.getItem("fav2"))
+      setFav2(valor2)
+      console.log(JSON.parse(localStorage.getItem("fav2")))
+    }
+
+    if (localStorage.getItem("fav3")==null) {
+      setFav3(false)
+    } else {
+      const valor3 = JSON.parse(localStorage.getItem("fav3"))
+      setFav3(valor3)
+      console.log(JSON.parse(localStorage.getItem("fav3")))
+    }
+
+    if (localStorage.getItem("fav4")==null) {
+      setFav4(false)
+    } else {
+      const valor4 = JSON.parse(localStorage.getItem("fav4"))
+      setFav4(valor4)
+      console.log(JSON.parse(localStorage.getItem("fav4")))
+    }
+  }
+
   const guardar1 = () =>{
+    console.log("->"+ JSON.parse(!favorito1))
     setFav1(!favorito1)
-    localStorage.setItem("fav1",favorito1)
-    console.log(localStorage.getItem("fav1"))
+    localStorage.setItem("fav1",!favorito1)
+    //console.log(localStorage.getItem("fav1"))
   }
   const guardar2 = () =>{
     setFav2(!favorito2)
-    localStorage.setItem("fav2",favorito2)
+    localStorage.setItem("fav2",!favorito2)
+
   }
   const guardar3 = () =>{
     setFav3(!favorito3)
-    localStorage.setItem("fav3",favorito3)
+    localStorage.setItem("fav3",!favorito3)
+
   }
   const guardar4 = () =>{
     setFav4(!favorito4)
-    localStorage.setItem("fav4",favorito4)
+    localStorage.setItem("fav4",!favorito4)
+
   }
   return (
     <div>
@@ -52,7 +80,7 @@ const Overview = () => {
         <div className="cont-favorite">
           <button onClick={guardar1}>
             {
-              favorito1 ? <i className="fa-solid fa-heart"></i> : <i className="fa-regular fa-heart"></i>
+              favorito1 ? <i className="fa-solid fa-heart"></i> : <i className="fa-regular fa-heart"></i>              
             }
           </button>
         </div>
